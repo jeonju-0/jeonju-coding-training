ed36f5e9db94ebeacfdc1789a7527d5f2bf35cb6
이상민 <wjswnpk@gmail.com>
2026-08-10 18:12:51 +0900

    [Passed] 재귀함수를 이용한 별 출력 (2ms, 1MB)

---
+++ trail2/재귀함수를 이용한 별 출력/star-output-with-recursive-function.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void func(int n) {
+    if (n == 0) {return;}
+
+    func(n-1);
+    while(n--) {
+        cout << '*';
+    }
+    cout << '\n';
+
+}
+
+int main() {
+    int n;
+    cin >> n;
+    func(n);
+    
+    return 0;
+}
